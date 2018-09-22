933fcbcf53bdf90a313a2da6b8b26464f15ab4fa
chalanika sewwandi <chalanikasewwandi@gmail.com>
2018-09-22 16:17:44 +0530

    game_step1_finished

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-/*var winner=0;
+
 
 function getWinner(){
+    var winner;
+
     var box1=document.getElementById("b1"),
         box2=document.getElementById("b2"),
         box3=document.getElementById("b3"),
@@ -9,24 +11,29 @@ function getWinner(){
         box6=document.getElementById("b6"),
         box7=document.getElementById("b7"),
         box8=document.getElementById("b8"),
-        box9=document.getElementById("b9")
+        box9=document.getElementById("b9");
 
-        if(box1.innerHTML != "null" && box1.innerHTML==box2.innerHTML && box2.innerHTML==box3.innerHTML || box4.innerHTML != "null" && box4.innerHTML==box5.innerHTML && box5.innerHTML==box6.innerHTML || box7.innerHTML != "null" && box7.innerHTML==box8.innerHTML && box8.innerHTML==box9.innerHTML  ){
+        if(box1.innerHTML != null && box1.innerHTML==box2.innerHTML && box2.innerHTML==box3.innerHTML || box4.innerHTML != null && box4.innerHTML==box5.innerHTML && box5.innerHTML==box6.innerHTML || box7.innerHTML != null && box7.innerHTML==box8.innerHTML && box8.innerHTML==box9.innerHTML  ){
             console.log('winner');
             winner=1;
         }
-        elseif(box1.innerHTML != "null" && box1.innerHTML==box4.innerHTML && box4.innerHTML==box7.innerHTML || box2.innerHTML != "null" && box2.innerHTML==box5.innerHTML && box5.innerHTML==box8.innerHTML || box3.innerHTML != "null" && box3.innerHTML==box6.innerHTML && box6.innerHTML==box9.innerHTML  ){
+        else if(box1.innerHTML != null && box1.innerHTML==box4.innerHTML && box4.innerHTML==box7.innerHTML || box2.innerHTML != null && box2.innerHTML==box5.innerHTML && box5.innerHTML==box8.innerHTML || box3.innerHTML != null && box3.innerHTML==box6.innerHTML && box6.innerHTML==box9.innerHTML  ){
             console.log('winner');
             winner=1;
         }
-        elseif(box1.innerHTML != "null" && box1.innerHTML==box5.innerHTML && box5.innerHTML==box9.innerHTML || box3.innerHTML != "null" && box3.innerHTML==box5.innerHTML && box5.innerHTML==box7.innerHTML ){
+        else if(box1.innerHTML != null && box1.innerHTML==box5.innerHTML && box5.innerHTML==box9.innerHTML || box3.innerHTML != null && box3.innerHTML==box5.innerHTML && box5.innerHTML==box7.innerHTML ){
             console.log('winner');
             winner=1;
         }
+        else{
+            winner=0;
+        }
+
+        return(winner);
     
 
 }
-*/
+
 
 
 
@@ -34,19 +41,21 @@ var player1=1;
 var player2=0;
 
 function changetext(id){
-  if(winner!=1){
+var won;
+  if(won!=1){
     if(id.innerHTML !=="x" && id.innerHTML!=="o"){
     if(player1==1){
         id.innerHTML='x';
         player2=1;
         player1=0;
-        getWinner();
+        won=getWinner();
     }
     else{
         id.innerHTML='o';
         player1=1;
         player2=0;
-        getWinner();
+        won=getWinner();
+        
     }
 }
 
