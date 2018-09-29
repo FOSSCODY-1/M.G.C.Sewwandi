166ca30fdecf8da184b020372f2688337820f663
chalanika sewwandi <chalanikasewwandi@gmail.com>
2018-09-29 14:23:44 +0530

    design_update

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 
-var player1 = 1;
-var player2 = 0;
-var won = 0;
-var countlimit = 9;
+var player1 = 1; /*player1 has 1st chance to play*/
+var player2 = 0;/*player2 has 2nd chance to play*/
+var won = 0; 
+var countlimit = 9; /*game is drawn after 9 terms*/
 var count = 0;
 
-function getWinner() {
+function getWinner() { /*To find winner*/
     var winner;
-    var box1 = document.getElementById("b1"),
+    /*Get the created button's data in html to javascript*/
+        var box1 = document.getElementById("b1"),
         box2 = document.getElementById("b2"),
         box3 = document.getElementById("b3"),
         box4 = document.getElementById("b4"),
@@ -18,29 +19,31 @@ function getWinner() {
         box9 = document.getElementById("b9");
 
     var turn = document.getElementById("turn");
-
-    if ((box1.innerHTML == 'x' || box1.innerHTML == 'o') && box1.innerHTML == box2.innerHTML && box2.innerHTML == box3.innerHTML || (box4.innerHTML == 'x' || box4.innerHTML == 'o') && box4.innerHTML == box5.innerHTML && box5.innerHTML == box6.innerHTML || (box7.innerHTML == 'x' || box7.innerHTML == 'o') && box7.innerHTML == box8.innerHTML && box8.innerHTML == box9.innerHTML) {
-        console.log('winner');
-        winner = 1;
+    /*check buttons in grid horizontally*/
+    if ((box1.innerHTML == 'X' || box1.innerHTML == 'o') && box1.innerHTML == box2.innerHTML && box2.innerHTML == box3.innerHTML || (box4.innerHTML == 'x' || box4.innerHTML == 'o') && box4.innerHTML == box5.innerHTML && box5.innerHTML == box6.innerHTML || (box7.innerHTML == 'x' || box7.innerHTML == 'o') && box7.innerHTML == box8.innerHTML && box8.innerHTML == box9.innerHTML) {
+        console.log('winner'); /*print winner in console*/
+        winner = 1; /*if a player won,winner is  1*/
 
 
 
     }
-    else if ((box1.innerHTML == 'x' || box1.innerHTML == 'o') && box1.innerHTML == box4.innerHTML && box4.innerHTML == box7.innerHTML || (box2.innerHTML == 'x' || box2.innerHTML == 'o') && box2.innerHTML == box5.innerHTML && box5.innerHTML == box8.innerHTML || (box3.innerHTML == 'x' || box3.innerHTML == 'o') && box3.innerHTML == box6.innerHTML && box6.innerHTML == box9.innerHTML) {
+    /*check buttons in grid  vertically*/
+    else if ((box1.innerHTML == 'X' || box1.innerHTML == 'O') && box1.innerHTML == box4.innerHTML && box4.innerHTML == box7.innerHTML || (box2.innerHTML == 'x' || box2.innerHTML == 'o') && box2.innerHTML == box5.innerHTML && box5.innerHTML == box8.innerHTML || (box3.innerHTML == 'x' || box3.innerHTML == 'o') && box3.innerHTML == box6.innerHTML && box6.innerHTML == box9.innerHTML) {
         console.log('winner');
         winner = 1;
     }
-    else if ((box1.innerHTML == 'x' || box1.innerHTML == 'o') && box1.innerHTML == box5.innerHTML && box5.innerHTML == box9.innerHTML || (box3.innerHTML == 'x' || box3.innerHTML == 'o') && box3.innerHTML == box5.innerHTML && box5.innerHTML == box7.innerHTML) {
+    /*check buttons in grid  diagonally*/
+    else if ((box1.innerHTML == 'X' || box1.innerHTML == 'O') && box1.innerHTML == box5.innerHTML && box5.innerHTML == box9.innerHTML || (box3.innerHTML == 'x' || box3.innerHTML == 'o') && box3.innerHTML == box5.innerHTML && box5.innerHTML == box7.innerHTML) {
         console.log('winner');
         winner = 1;
     }
     else {
-        winner = 0;
+        winner = 0;/*if player is not win yet*/
     }
 
 
 
-    return (winner);
+    return (winner);/*return winner's value to the calling function*/
 
 
 }
@@ -51,14 +54,14 @@ function getWinner() {
 
 function changetext(id) {
 
-    if (count < countlimit) {
+    if (count < countlimit) /*if no of rounds is not finished*/ {
 
-        if (won != 1) {
-
-            if (id.innerHTML !== "x" && id.innerHTML !== "o") {
-                count++;
-                if (player1 == 1) {
-                    id.innerHTML = 'x';
+        if (won != 1)  /*if player didn't win game*/ {
+      
+            if (id.innerHTML !== "X" && id.innerHTML !== "O") /*if the buttons has nothing*/{
+                count++;/*increase no of round by 1*/
+                if (player1 == 1) /*palyer1 has the turn now*/{
+                    id.innerHTML = 'X';/*display X on the button */
                     var turn = document.getElementById('turn');
                     player2 = 1;
                     player1 = 0;
@@ -67,19 +70,19 @@ function changetext(id) {
                     if (count <= countlimit) {
                         if (won == 1) {
 
-                            turn.innerHTML = "player X won";
-                            alert("player X won!!!");
+                            turn.innerHTML = "Player X Won.";/*display winner*/
+                            alert("Player1 won!!!");
 
                         }
                         else {
                             if (count < countlimit) {
-                                turn.innerHTML = "O turn now";
+                                turn.innerHTML = "O Turn Now.";/*give next turn to player2*/
 
 
                             }
                             else {
-                                turn.innerHTML = "Draw";
-                                alert("Game is drawn!!!");
+                                turn.innerHTML = "Draw.";/*rounds are finished*/
+                                alert("Game is drawn!!!"); 
                                 count = 0;
                             }
                         }
@@ -88,7 +91,7 @@ function changetext(id) {
 
                     }
                     else {
-                        turn.innerHTML = "Draw";
+                        turn.innerHTML = "Draw.";
                         alert("Game is drawn!!!");
                         count = 0;
 
@@ -97,8 +100,8 @@ function changetext(id) {
                     }
 
                 }
-                else {
-                    id.innerHTML = 'o';
+                else/*player2 has the trun now*/ {
+                    id.innerHTML = 'O';
                     var turn = document.getElementById("turn");
 
                     player1 = 1;
@@ -107,17 +110,17 @@ function changetext(id) {
 
                     if (count <= countlimit) {
                         if (won == 1) {
-                            turn.innerHTML = 'player o won';
+                            turn.innerHTML = 'Player O Won.';
 
-                            alert("player O won!!!");
+                            alert("Player2 won!!!");
 
                         }
                         else {
                             if (count < countlimit) {
-                                turn.innerHTML = "x turn now";
+                                turn.innerHTML = "X Turn Now.";
                             }
                             else {
-                                turn.innerHTML = "Draw";
+                                turn.innerHTML = "Draw.";
                                 alert("Game is drawn!!!")
                                 count = 0;
                             }
@@ -143,7 +146,7 @@ function changetext(id) {
 
 
 
-function restart() {
+function restart()/*restart game*/ {
 
     player1 = 1;
     player2 = 0;
@@ -160,7 +163,7 @@ function restart() {
         box9 = document.getElementById("b9");
 
 
-
+/*set all buttons without any data*/
     box1.innerHTML = null;
     box2.innerHTML = null;
     box3.innerHTML = null;
@@ -172,7 +175,7 @@ function restart() {
     box9.innerHTML = null;
 
 
-    turn.innerHTML = 'play';
+    turn.innerHTML = 'Play';
     count = 0;
 
 }
